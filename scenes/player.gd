97037875941fb613b5b0d20f3a3d11892f0d54e5
Arya Allermoz <arya.allermoz@gmail.com>
2026-04-22 22:41:11 +0200

    Initial commit

---
+++ scenes/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+
+const SPEED = 32.0
+const UP: Vector2i = Vector2i(0,-1)
+const DOWN: Vector2i = Vector2i(0,1)
+const LEFT: Vector2i = Vector2i(-1,0)
+const RIGHT: Vector2i = Vector2i(1,0)
+
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.
