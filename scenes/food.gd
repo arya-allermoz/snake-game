6d780e825f0309bbd397a2cfeb3697baee906f27
Arya Allermoz <arya.allermoz@gmail.com>
2026-04-23 11:23:28 +0200

    Fix: removed unnecessary code and added comments

--- scenes/food.gd
+++ scenes/food.gd
@@ -2,14 +2,7 @@ extends Area2D
 
 signal food_consumed
 
-var is_consumed: bool = false
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
-
 func _on_body_entered(_body: Node2D) -> void:
+	# when the player overlaps the food, generates new food instance and delete this instance
 	food_consumed.emit()
-	is_consumed = true
 	call_deferred("queue_free")
--- scenes/level.gd
+++ scenes/level.gd
@@ -4,22 +4,23 @@ var food_scene: PackedScene = preload("res://scenes/food.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#spawns the initial food
 	spawn_food()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
-
+# handles spawning a new instance of food randomly on the map
 func spawn_food() -> void:
-	# creates new food instance and conencts the signals
+	# creates new food instance and connects the signals
 	var food = food_scene.instantiate() as Area2D
 	food.connect("food_consumed", _on_food_consumed)
 	
-	# randomizes it's position while staying on the grid
+	# randomizes its position while staying aligned to the grid
 	food.position = Vector2i( (randi()%21)*32 - 16, ((randi()%12)*32 - 12) )
+	
+	# adds the instance to the scene tree to make it appear
 	add_child(food)
 
+
+# handles calling all other methods when food is consumed
 func _on_food_consumed() -> void:
 	call_deferred("spawn_food")
